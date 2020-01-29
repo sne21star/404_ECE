53272ab2f671f83abc148eb050126650febef4cc
snehamahapatra <mahapat0@purdue.edu>
2020-01-29 16:38:19 -0500

    Everything Included

--- Lecture3Code/DES_image.py
+++ Lecture3Code/DES_image.py
@@ -6,7 +6,7 @@ Due Date: 01/30/2020
 """
 #!/usr/bin/env python
 #!/usr/bin/env python -W ignore::DeprecationWarning
-### hw2_starter.py
+### DES_text.py
 import codecs
 import sys
 import io
@@ -16,7 +16,8 @@ import BitVector
 from math import *
 from generate_round_keys import *
 from illustrate_des_substitution import *
-
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
 expansion_permutation = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0]
 p_box_permutation = [15,6,19,20,28,11,27,16,0,14,22,25,4,17,30,9,1,7,23,13,31,26,2,8,18,12,29,5,21,10,3,24]
 SIZE = 64
@@ -28,8 +29,8 @@ def encrypt():
 
     image = open(sys.argv[2], 'rb')
     imageno = image.readline()
-    height = image.readline()
-    width = image.readline()
+    height_width = image.readline()
+    max_pixVal = image.readline()
     bv = BitVector(filename=sys.argv[2])
     image.close()
 
@@ -38,15 +39,13 @@ def encrypt():
 
     text_file = open(sys.argv[4], "wb")
     text_file.write(imageno)
-    text_file.write(height)
-    text_file.write(width)
+    text_file.write(height_width)
+    text_file.write(max_pixVal)
     while (bv.more_to_read):
         bitvec = bv.read_bits_from_file(SIZE)
         if (len(str(bitvec)) % SIZE != 0):
             x = bitvec.length() % 64
             bitvec.pad_from_right(SIZE - x)
-            print(bitvec)
-            print(bitvec.length())
         if (len(str(bitvec)) > 0):
             [LE, RE] = bitvec.divide_into_two()
             for keyR in round_key:
--- Lecture3Code/DES_text.py
+++ Lecture3Code/DES_text.py
@@ -5,15 +5,16 @@ ECN Login: mahapat0
 Due Date: 01/30/2020
 """
 # !/usr/bin/env python
-# !/usr/bin/env python -W ignore::DeprecationWarning
-### hw2_starter.py
+# !/usr/bin/env python -W ignore:tostring:DeprecationWarning
+### DES_text.py
 import codecs
 import sys
 import BitVector
 # from get_encryption_key import *
 from generate_round_keys import *
 from illustrate_des_substitution import *
-
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
 expansion_permutation = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17,18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0]
 p_box_permutation = [15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24]
 SIZE = 64
@@ -29,10 +30,8 @@ def encrypt():
     while (bv.more_to_read):
         bitvec = bv.read_bits_from_file(SIZE)
         if (len(str(bitvec)) % SIZE != 0):
-            x = bitvec.length() % 64
+            x = bitvec.length() % SIZE
             bitvec.pad_from_right(SIZE-x)
-            print(bitvec)
-            print(bitvec.length())
         if (len(str(bitvec)) > 0):
             [LE, RE] = bitvec.divide_into_two()
             for keyR in round_key:
@@ -44,8 +43,6 @@ def encrypt():
                 LE = LE ^ round_i
                 RE = LE
                 LE = temp
-        print(LE)
-        print(RE)
         bitX = RE + LE
         myhexstring = bitX.get_bitvector_in_hex()
         text_file.write(myhexstring)
@@ -76,10 +73,7 @@ def decrypt():
     totalList = []
     while (index < secOfBits):
         bitvec = BitVector(bitlist=bvList[index1:index1 + SIZE])
-        print(bitvec.length())
         [LE, RE] = bitvec.divide_into_two()
-        print(LE)
-        print(RE)
         for keyR in round_key:
             temp = RE
             newRE = RE.permute(expansion_permutation)
@@ -96,11 +90,10 @@ def decrypt():
             for k in strC:
                 if(k.isprintable()):
                     text_file.write(k)
-            print(strX)
         else:
             text_file.write(bitX.get_text_from_bitvector())
         index += 1
-        index1 += 64
+        index1 += SIZE
     text_file.close()
     pass
 
