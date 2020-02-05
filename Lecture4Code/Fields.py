87da194ac519ae7ba1ddd3d5eae8ed6f3f708216
snehamahapatra <mahapat0@purdue.edu>
2020-02-05 16:23:28 -0500

    Changes

--- Lecture4Code/Fields.py
+++ Lecture4Code/Fields.py
@@ -1,12 +1,23 @@
+'''
+Homework Number: 3
+Name: Sneha Mahapatra
+ECN Login: mahapat0
+Due Date: 02/03/2020
+'''
+from sympy import *
+# Constants
+prime50 = dict({2: 1, 3: 1, 5: 1, 7: 1, 11: 1, 13: 1, 17: 1, 19: 1, 23: 1, 29: 1, 31: 1, 37: 1, 41: 1, 43: 1, 47: 1})
+
+
 def getinput():
 	gotNumber = False
 	while not gotNumber:
 		num = input("Enter a number: ")
 		num = float(num)
 		if num >= 50:
-			print("Please enter an integer between 0 and 50")
+			print("Please enter an integer [0,50)")
 		elif num < 0:
-			print("Please enter an integer between 0 and 50")
+			print("Please enter an integer [0,50)")
 		elif not num.is_integer():
 			print("Number must be an Integer")
 		else:
@@ -14,9 +25,12 @@ def getinput():
 	return int(num)
 
 
-# return true or false
+def isprime(num):
+	return prime50.get(num)
+
+
 def isfield(num):
-	return False
+	return isprime(num)
 
 
 def main():
@@ -24,10 +38,9 @@ def main():
 	isField = isfield(num)
 
 	if isField:
-		print(str(num) + " is a Field")
+		print("Z_" + str(num) + " is a Field")
 	else:
-		print(str(num) + " is a Commutative Ring only")
-
+		print("Z_" + str(num) + " is a Commutative Ring only")
 
 if __name__ == "__main__":
 	main()
