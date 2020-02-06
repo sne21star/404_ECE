c6530b9bbde8cc38380bfde386419ca8d7ba596c
snehamahapatra <mahapat0@purdue.edu>
2020-02-05 19:13:12 -0500

    Fermats Theroem

--- Lecture4Code/Fields.py
+++ Lecture4Code/Fields.py
@@ -2,40 +2,36 @@
 Homework Number: 3
 Name: Sneha Mahapatra
 ECN Login: mahapat0
-Due Date: 02/03/2020
+Due Date: 02/05/2020
 '''
-# Constants
-prime50 = dict({2: 1, 3: 1, 5: 1, 7: 1, 11: 1, 13: 1, 17: 1, 19: 1, 23: 1, 29: 1, 31: 1, 37: 1, 41: 1, 43: 1, 47: 1})
-
-
 def getinput():
 	gotNumber = False
 	while not gotNumber:
-		num = input("Enter a number: ")
-		num = float(num)
-		if num >= 50:
-			print("Please enter an integer [0,50)")
-		elif num < 0:
-			print("Please enter an integer [0,50)")
-		elif not num.is_integer():
+		given_num = input("Enter a number: ")
+		given_num = float(given_num)
+		if not given_num.is_integer():
 			print("Number must be an Integer")
 		else:
 			gotNumber = True
-	return int(num)
+	return int(given_num)
 
-
-def isprime(num):
-	return prime50.get(num)
-
+#Used fermat little primality, a probability test to check whether or not it is prime
+def isprime(given_num):
+	if given_num < 2:
+		return False
+	elif given_num == 2:
+		return True
+	elif given_num%2 == 0:
+		return False
+	isPrime = (2 ** given_num-1) % given_num == 1
+	return isPrime
 
 def isfield(num):
 	return isprime(num)
 
-
 def main():
-	num = getinput()
-	isField = isfield(num)
-
+	given_num = getinput()
+	isField = isfield(given_num)
 	if isField:
 		print("field")
 	else:
