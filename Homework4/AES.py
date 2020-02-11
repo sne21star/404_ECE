699024c089676a830add8d1ff7a3612eac688c81
snehamahapatra <mahapat0@purdue.edu>
2020-02-10 23:55:33 -0500

    Homework 4 Initial

---
+++ Homework4/AES.py
@@ -0,0 +1,139 @@
+"""
+Homework Number: 4
+Name: Sneha Mahapatra
+ECN Login: mahapat0
+Due Date: 02/18/2020
+"""
+#python AES.py -e message.txt key.txt encrypted.txt
+#python AES.py -d encrypted.txt key.txt decrypted.txt
+from sys import *
+from BitVector import *
+AES_modulus = BitVector(bitstring='100011011')
+
+subBytesTable = []                                                  # for encryption
+invSubBytesTable = []                                               # for decryption
+
+SIZE = 256
+
+def encrypt():
+    #Read Key
+    READKEY = open(sys.argv[3], 'r')
+    key = READKEY.read()
+    READKEY.close()
+
+    #Turn File into BitVector
+    bv = BitVector(filename=sys.argv[2])
+
+    #Open File for Encrypted Text
+    encryptedText = open(sys.argv[4], "w")
+
+    # For loop for 10 rounds
+    # SubBytes
+    # ShiftRows
+    # Mix Columns  --> Not on last round
+    # Add Round Keys
+    while(bv.more_to_read):
+        bitvec = bv.read_bits_from_file(SIZE)
+        numRounds = 10
+        while(numRounds > 1):
+            numRounds -= 1
+            pass
+
+        if(numRounds == 1):
+            pass
+
+    encryptedText.close()
+    print("encrypt")
+    pass
+def gee(keyword, round_constant, byte_sub_table):
+    '''
+    This is the g() function you see in Figure 4 of Lecture 8.
+    '''
+    rotated_word = keyword.deep_copy()
+    rotated_word << 8
+    newword = BitVector(size = 0)
+    for i in range(4):
+        newword += BitVector(intVal = byte_sub_table[rotated_word[8*i:8*i+8].intValue()], size = 8)
+    newword[:8] ^= round_constant
+    round_constant = round_constant.gf_multiply_modular(BitVector(intVal = 0x02), AES_modulus, 8)
+    return newword, round_constant
+def gen_key_schedule_256(key_bv):
+    byte_sub_table = gen_subbytes_table()
+    #  We need 60 keywords (each keyword consists of 32 bits) in the key schedule for
+    #  256 bit AES. The 256-bit AES uses the first four keywords to xor the input
+    #  block with.  Subsequently, each of the 14 rounds uses 4 keywords from the key
+    #  schedule. We will store all 60 keywords in the following list:
+    key_words = [None for i in range(60)]
+    round_constant = BitVector(intVal = 0x01, size=8)
+    for i in range(8):
+        key_words[i] = key_bv[i*32 : i*32 + 32]
+    for i in range(8,60):
+        if i%8 == 0:
+            kwd, round_constant = gee(key_words[i-1], round_constant, byte_sub_table)
+            key_words[i] = key_words[i-8] ^ kwd
+        elif (i - (i//8)*8) < 4:
+            key_words[i] = key_words[i-8] ^ key_words[i-1]
+        elif (i - (i//8)*8) == 4:
+            key_words[i] = BitVector(size = 0)
+            for j in range(4):
+                key_words[i] += BitVector(intVal =
+                                 byte_sub_table[key_words[i-1][8*j:8*j+8].intValue()], size = 8)
+            key_words[i] ^= key_words[i-8]
+        elif ((i - (i//8)*8) > 4) and ((i - (i//8)*8) < 8):
+            key_words[i] = key_words[i-8] ^ key_words[i-1]
+        else:
+            sys.exit("error in key scheduling algo for i = %d" % i)
+    return key_words
+
+def gen_subbytes_table():
+    subBytesTable = []
+    c = BitVector(bitstring='01100011')
+    for i in range(0, 256):
+        a = BitVector(intVal = i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
+        a1,a2,a3,a4 = [a.deep_copy() for x in range(4)]
+        a ^= (a1 >> 4) ^ (a2 >> 5) ^ (a3 >> 6) ^ (a4 >> 7) ^ c
+        subBytesTable.append(int(a))
+    return subBytesTable
+
+def genTables():
+    c = BitVector(bitstring='01100011')
+    d = BitVector(bitstring='00000101')
+    for i in range(0, 256):
+        # For the encryption SBox
+        a = BitVector(intVal = i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
+        # For bit scrambling for the encryption SBox entries:
+        a1,a2,a3,a4 = [a.deep_copy() for x in range(4)]
+        a ^= (a1 >> 4) ^ (a2 >> 5) ^ (a3 >> 6) ^ (a4 >> 7) ^ c
+        subBytesTable.append(int(a))
+        # For the decryption Sbox:
+        b = BitVector(intVal = i, size=8)
+        # For bit scrambling for the decryption SBox entries:
+        b1,b2,b3 = [b.deep_copy() for x in range(3)]
+        b = (b1 >> 2) ^ (b2 >> 5) ^ (b3 >> 7) ^ d
+        check = b.gf_MI(AES_modulus, 8)
+        b = check if isinstance(check, BitVector) else 0
+        invSubBytesTable.append(int(b))
+'''
+genTables()
+print("SBox for Encryption:")
+print(subBytesTable)
+print("SBox for Decryption:")
+print(invSubBytesTable)
+'''
+def decrypt():
+    print("decrypt")
+    pass
+
+def main():
+    charX = sys.argv[1]
+    if (charX == '-e'):
+        encrypt()
+    elif (charX == '-d'):
+        decrypt()
+    else:
+        print("Either -e or -d")
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Homework4/DES_text.py
@@ -0,0 +1,106 @@
+"""
+Homework Number: 2
+Name: Sneha Mahapatra
+ECN Login: mahapat0
+Due Date: 01/30/2020
+"""
+# !/usr/bin/env python
+# !/usr/bin/env python -W ignore:tostring:DeprecationWarning
+### DES_text.py
+import codecs
+import sys
+import BitVector
+# from get_encryption_key import *
+from generate_round_keys import *
+from illustrate_des_substitution import *
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
+expansion_permutation = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17,18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0]
+p_box_permutation = [15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24]
+SIZE = 64
+
+def encrypt():
+    FILEREAD = open(sys.argv[3], 'r')
+    key = FILEREAD.read()
+    FILEREAD.close()
+    keyBit = get_encryption_key(key)
+    round_key = generate_round_keys(keyBit)
+    bv = BitVector(filename=sys.argv[2])
+    text_file = open(sys.argv[4], "w")
+    while (bv.more_to_read):
+        bitvec = bv.read_bits_from_file(SIZE)
+        if (len(str(bitvec)) % SIZE != 0):
+            x = bitvec.length() % SIZE
+            bitvec.pad_from_left(SIZE-x)
+        if (len(str(bitvec)) > 0):
+            [LE, RE] = bitvec.divide_into_two()
+            for keyR in round_key:
+                temp = RE
+                newRE = RE.permute(expansion_permutation)
+                out_xor = newRE ^ keyR
+                output = substitute(out_xor)
+                round_i = output.permute(p_box_permutation)
+                LE = LE ^ round_i
+                RE = LE
+                LE = temp
+        bitX = RE + LE
+        myhexstring = bitX.get_bitvector_in_hex()
+        text_file.write(myhexstring)
+    text_file.close()
+    pass
+
+def decrypt():
+    FILEREAD = open(sys.argv[3], 'r')
+    key = FILEREAD.read()
+    FILEREAD.close()
+    keyBit = get_encryption_key(key)
+    round_key = generate_round_keys(keyBit)
+    round_key = round_key[::-1]
+
+    FILEHEX = open(sys.argv[2], 'r')
+    hexString = FILEHEX.read()
+    bv = BitVector(hexstring=hexString)
+
+    bvList = list(bv)
+    FILEHEX.close()
+
+    text_file = open(sys.argv[4], "w")
+
+    secOfBits = bv.length() / SIZE
+    index = 0
+    index1 = 0
+    totalList = []
+    while (index < secOfBits):
+        bitvec = BitVector(bitlist=bvList[index1:index1 + SIZE])
+        [LE, RE] = bitvec.divide_into_two()
+        for keyR in round_key:
+            temp = RE
+            newRE = RE.permute(expansion_permutation)
+            out_xor = newRE ^ keyR
+            output = substitute(out_xor)
+            round_i = output.permute(p_box_permutation)
+            LE = round_i ^ LE
+            RE = LE
+            LE = temp
+        bitX = RE + LE
+        text_file.write(bitX.get_text_from_bitvector())
+        index += 1
+        index1 += SIZE
+    text_file.close()
+    pass
+
+def main():
+    charX = sys.argv[1]
+    print(charX)
+    if (charX == '-e'):
+        encrypt()
+    elif (charX == '-d'):
+        decrypt()
+    else:
+        print("Either -e or -d")
+    pass
+
+
+if __name__ == "__main__":
+    main()
+# Convert Image to Bit vector and get rid of first three lines
